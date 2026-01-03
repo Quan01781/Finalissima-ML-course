f2d1cf9bc00ac627ab22dc586ea1282f38e9fa31
Quan01781 <56251630+Quan01781@users.noreply.github.com>
2026-01-03 14:09:16 +0700

    Add files via upload

--- FinalissimaG4/Config/config.py
+++ FinalissimaG4/Config/config.py
@@ -1,33 +1,33 @@
 class Config:
-    PRETRAIN_STAGE = False
+    PRETRAIN_STAGE = True
     # Dataset
     # DATASET_ROOT = "dataset"
     DATASET_ROOT = "kaggle_dataset" if PRETRAIN_STAGE == True else "dataset"
     NUM_CLASSES = 2 if PRETRAIN_STAGE == True else 4
 
     # Ablation study
-    USE_RESNET_SE = False
+    USE_RESNET_SE = True
 
     # Label Smoothing
-    # LABEL_SMOOTHING = 0.05
+    # LABEL_SMOOTHING = 0.01
     
     # Training
     IMG_SIZE = 224
     BATCH_SIZE = 16
     EPOCHS = 20 if PRETRAIN_STAGE == True else 30
     LR = 3e-4
-    FREEZE_EPOCHS = 5      #  epochs freeze backbone
-    PATIENCE = 5           #  early stopping
+    FREEZE_EPOCHS = 6      #  epochs freeze backbone
+    PATIENCE = 2 if PRETRAIN_STAGE == True else 5    #  early stopping
 
     # Model
     MODELS = [
         "efficientnet_b0", # EfficiemtNet
-        # "mobilenetv3_small_100", # MobileNet 
-        # "shufflenet",
+        "mobilenetv3_small_100", # MobileNet 
+        "shufflenet",
         # "resnetse" 
         ]
-    # Default model (fallback)
-    MODEL_NAME = "efficientnet_b0"
+    
+    MODEL_NAME = "resnetse"
 
     # Device
     DEVICE = "cuda"
